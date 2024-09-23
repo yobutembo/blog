a6acd69f89ccf7aeb255fe91d2a41563d7dc8b5b
Yobu <121264075+yobutembo@users.noreply.github.com>
2024-09-23 22:54:35 +0530

    Setup mongodb using mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 dotenv.config();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
