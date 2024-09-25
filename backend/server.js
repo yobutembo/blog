c6d5da8c97d44dadbcd2a4051c29c6e419c3f35c
Yobu <121264075+yobutembo@users.noreply.github.com>
2024-09-25 22:10:03 +0530

    feat: Add a custom asyncHandler to eliminate try-catch

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,34 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import BlogPost from "./models/blogPostModel.js";
 import User from "./models/userModel.js";
+import asyncHandler from "./middleWare/asyncHandler.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 dotenv.config();
-
 connectDB();
 
 app.get("/", (req, res) => {
   res.send("App is running");
 });
 
-app.get("/api/allblogs", async (req, res) => {
-  try {
+app.get(
+  "/api/blogs",
+  asyncHandler(async (req, res) => {
     const blogs = await BlogPost.find({});
     res.json(blogs);
-  } catch (error) {
-    console.error(error);
-  }
-});
+  })
+);
+
+app.get(
+  "/api/blogs/:id",
+  asyncHandler(async (req, res) => {
+    const blog = await BlogPost.findById(req.params.id);
+    res.json(blog);
+  })
+);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
